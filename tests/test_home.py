2e0ad8fd6b22b2e76c3e0d2e69442675dfdba42b
Romain Clement <r.clement88@gmail.com>
2018-01-08 00:01:17 +0100

    Add test for initial homepage without posts

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,7 +3,7 @@ import unittest
 from flask_testing import TestCase
 from carnet import create_app
 
-from tests.utils import delete_app_config
+from tests.utils import create_app_config, delete_app_config
 
 
 class TestHome(TestCase):
@@ -20,6 +20,14 @@ class TestHome(TestCase):
         rv = self.client.get('/', follow_redirects=True)
         self.assert_200(rv)
         self.assert_template_used('quickstart.html')
+
+    def test_empty_latest_posts(self):
+        create_app_config()
+
+        rv = self.client.get('/')
+        self.assert_200(rv)
+        self.assert_template_used('latest_posts.html')
+        self.assertIn(b'No posts', rv.data)
 
 
 if __name__ == '__main__':
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,25 @@
 import os
+import json
 
 from carnet.utils.config import get_app_config_path
+
+
+def test_app_config():
+    return {
+        'title': 'My Title',
+        'subtitle': 'My subtitle',
+        'author': 'Me, Myself and I',
+        'theme': '',
+        'pages_path': 'pages',
+        'posts_path': 'posts',
+        'output_path': 'output',
+    }
+
+
+def create_app_config():
+    app_config_path = get_app_config_path()
+    with open(app_config_path, mode='w', encoding='utf-8') as f:
+        json.dump(test_app_config(), f, indent=4)
 
 
 def delete_app_config():
