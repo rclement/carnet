c3f3ba45e015a4748bcf81e6e772669585c97ccf
Romain Clement <r.clement88@gmail.com>
2018-01-23 00:32:01 +0100

    Add unit-test for freeze operation

---
+++ tests/test_freeze.py
@@ -0,0 +1,20 @@
+import unittest
+
+from warnings import simplefilter as filter_warnings
+from flask_frozen import MissingURLGeneratorWarning
+
+from carnet import freezer
+from tests.utils import TestAppConfigured
+
+
+filter_warnings('ignore', MissingURLGeneratorWarning)
+
+
+class TestFreezeConfigured(TestAppConfigured):
+
+    def test_freeze(self):
+        freezer.freeze()
+
+
+if __name__ == '__main__':
+    unittest.main()
