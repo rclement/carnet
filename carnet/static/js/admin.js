3b67c3ff5fd16d5d51fe5178c6c7694208137953
Romain Clement <r.clement88@gmail.com>
2018-02-07 23:27:43 +0100

    Start adding admin pages listing and editing

--- carnet/static/js/admin.js
+++ carnet/static/js/admin.js
@@ -46,23 +46,91 @@ const Dashboard = {
         <div id="dashboard">
             <div class="row">
                 <div class="col-md-12">
-                <h2>Dashboard</h2>
+                    <h2>Dashboard</h2>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" :value="config.title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="subtitle">Subtitle</label>
+                            <input type="text" class="form-control" id="subtitle" :value="config.subtitle">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" class="form-control" id="author" :value="config.author">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </form>
                 </div>
             </div>
         </div>
-    `
+    `,
+
+    data: function() {
+        return {
+            config: global_config
+        }
+    }
 }
+
+/* -------------------------------------------------------------------------- */
 
 const Posts = {
     template: `
         <div id="posts">
             <div class="row">
                 <div class="col-md-12">
-                <h2>Posts</h2>
+                    <h2>Posts</h2>
                 </div>
             </div>
         </div>
     `
+}
+
+/* -------------------------------------------------------------------------- */
+
+const PageEdit = {
+    template: `
+        <div id="page">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Page Edit</h2>
+
+                    <p>Last edit on {{ page.published }}.</p>
+
+                    <form>
+                        <div class="form-group">
+                            <label for="pageTitle">Title</label>
+                            <input type="text" class="form-control" id="pageTitle" :value="page.title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pageAuthor">Author</label>
+                            <input type="text" class="form-control" id="pageAuthor" :value="page.author">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pageBody">Body</label>
+                            <textarea class="form-control" id="pageBody" rows="10">{{ page.body }}</textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    `,
+
+    props: ['page']
 }
 
 const Pages = {
@@ -70,11 +138,46 @@ const Pages = {
         <div id="pages">
             <div class="row">
                 <div class="col-md-12">
-                <h2>Pages</h2>
+                    <h2>Pages</h2>
+
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                    <th>Published</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="p in config.all_pages"
+                                    v-on:click="editPage(p)">
+                                    <td>{{ p.title }}</td>
+                                    <td>{{ p.author }}</td>
+                                    <td>{{ p.published }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    `
+    `,
+
+    data: function() {
+        return {
+            config: global_config
+        }
+    },
+
+    methods: {
+        editPage: function(page) {
+            this.$router.push({
+                name: 'PageEdit',
+                params: { id: page.path }
+            })
+        }
+    }
 }
 
 /* -------------------------------------------------------------------------- */
@@ -110,18 +213,16 @@ const App = {
                                 <router-link :to="{ name: 'Dashboard' }">Dashboard</router-link>
                             </li>
                             <li role="presentation" :class="{ active: currentRoute === 'Posts' }">
-                                <router-link :to="{ name: 'Posts' }">Posts</router-link>
+                                <router-link :to="{ name: 'Posts' }">Posts ({{ config.all_posts.length }})</router-link>
                             </li>
                             <li role="presentation" :class="{ active: currentRoute === 'Pages' }">
-                                <router-link :to="{ name: 'Pages' }">Pages</router-link>
+                                <router-link :to="{ name: 'Pages' }">Pages ({{ config.all_pages.length }})</router-link>
                             </li>
                         </ul>
 
-                        <div class="container">
-                            <transition name="router-fade" mode="out-in">
-                                <router-view></router-view>
-                            </transition>
-                        </div>
+                        <transition name="router-fade" mode="out-in">
+                            <router-view></router-view>
+                        </transition>
                     </div>
                 </div>
             </div>
@@ -130,6 +231,7 @@ const App = {
 
     data: function() {
         return {
+            config: global_config,
             flashedMessages: flashedMessages
         }
     },
@@ -147,6 +249,7 @@ const routes = [
     { path: '/', component: Dashboard, name: 'Dashboard' },
     { path: '/posts', component: Posts, name: 'Posts' },
     { path: '/pages', component: Pages, name: 'Pages' },
+    { path: '/pages/edit/:id', component: PageEdit, name: 'PageEdit', props: (route) => ({ page: _all_pages[route.params.id] }) },
     { path: "*", component: PageNotFound, name: 'PageNotFound' }
 ]
 
